e16dc85a17bcc0debfb95e2931b67a9760b79298
Damian Jaworski <djaaworski@gmail.com>
2023-03-08 14:42:34 +0100

    Fixing imports swagger-ui integration

--- src/main/java/com/example/mostgenericappwithapis/router/FirstApiRouter.java
+++ src/main/java/com/example/mostgenericappwithapis/router/FirstApiRouter.java
@@ -2,6 +2,8 @@ package com.example.mostgenericappwithapis.router;
 
 
 import com.example.mostgenericappwithapis.handlers.FirstApiHandler;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.webflux.core.fn.SpringdocRouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +12,8 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springdoc.webflux.core.fn.SpringdocRouteBuilder.route;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,15 +23,17 @@ public class FirstApiRouter {
     @Bean
     public RouterFunction<ServerResponse> firstRoute() {
         return RouterFunctions.nest(path("/v1"), route()
-                .GET("/hello")
-                        .and(accept(MediaType.APPLICATION_JSON)),
+                .GET("/hello",
                         firstApiHandler::hello,
-                       ops -> ops.tag("Swagger group")
-                       .summary("Swagger summary")
-                       .operationId("API function ID")
-                       //.parameter(parameterBuilder().name("param-name").required(false).description("Param description").implementation(String.class).in(ParameterIn.QUERY).example("example value"))
+                        ops -> ops.tag("Swagger group")
+                        .summary("Swagger summary")
+                        .operationId("API function ID")
+//                        .and(accept(MediaType.APPLICATION_JSON)),
+
+                        //.parameter(parameterBuilder().name("param-name").required(false).description("Param description").implementation(String.class).in(ParameterIn.QUERY).example("example value"))
                        //.response(responseBuilder().responseCode("200").content(contentBuilder().mediaType.APPLICATION_JSON_VALUE).schema(schemaBuilder().implementation(Long.class))))
                        //.response(responseBuilder().responseCode("400").description("Bad Request").implementation(ResponseError.class))
-                        );
+                        ).build()
+        );
     }
 }
