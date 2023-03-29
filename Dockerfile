4f5f32a99e78aa08c3d5a887127891c7f8b4b669
Damian Jaworski <djaaworski@gmail.com>
2023-03-29 14:57:45 +0200

    Testing solutions for docker build with testcontainers

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,29 @@
-FROM gradle:8-jdk11-alpine AS build
-COPY build.gradle .
+FROM docker:20.10.21-dind-alpine3.17 AS build
+RUN apk add openjdk11
+COPY gradlew ./
 COPY src ./src
-RUN gradle build
-RUN ls -l ./build/libs
-RUN pwd
+COPY db ./db
+#VOLUME //var/run/docker.sock
+#ENV TESTCONTAINERS_HOST_OVERRIDE=host.docker.internal
+#ENV DOCKER_HOST=tcp://localhost:2375
+RUN #ls -l /var/run/docker.sock
+RUN #docker ps
+RUN #curl --unix-socket /var/run/docker.sock http:/_/_ping
+RUN #ls -al; return 1
+RUN ls ./gradlew
+RUN chmod +x gradlew
+RUN which java
+RUN set -x
+RUN ls -l gradlew
+RUN dos2unix gradlew
+RUN ls -l gradlew
+RUN head -n1 gradlew
+RUN gradlew clean build
+#ENTRYPOINT ["gradle", "clean", "build"]
+#RUN ls -l ./build/libs
+#RUN pwd
 
-
-FROM openjdk:11.0.1-jdk-slim
-COPY --from=build /home/gradle/build/libs/* file.jar
-ENTRYPOINT ["java", "-jar", "-Dspring.profiles.active=docker", "file.jar"]
+#
+#FROM openjdk:11.0.15-jdk-slim
+#COPY --from=build /home/gradle/build/libs/* file.jar
+#ENTRYPOINT ["java", "-jar", "-Dspring.profiles.active=docker", "file.jar"]
--- src/test/java/com/example/mostgenericappwithapis/config/FlywayConfig.java
+++ src/test/java/com/example/mostgenericappwithapis/config/FlywayConfig.java
@@ -5,9 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @Configuration
-@Profile("!test")
+@Profile("test")
 public class FlywayConfig {
 
     private final Environment env;
@@ -18,12 +19,17 @@ public class FlywayConfig {
 
     @Bean(initMethod = "migrate")
     public Flyway flyway() {
+        String POSTGRES_VERSION = "postgres:11.1";
+        PostgreSQLContainer database;
+
+        database = new PostgreSQLContainer(POSTGRES_VERSION);
+        database.start();
         return new Flyway(Flyway.configure()
                 .baselineOnMigrate(true)
                 .dataSource(
-                        env.getRequiredProperty("spring.flyway.url"),
-                        env.getRequiredProperty("spring.flyway.user"),
-                        env.getRequiredProperty("spring.flyway.password"))
+                        database.getJdbcUrl(),
+                        database.getUsername(),
+                        database.getPassword())
                 .locations(env.getRequiredProperty("spring.flyway.locations"))
         );
     }
